2c93523d1315707d8af81bf2b860a0b3076634af
Sid <sepner@vmware.com>
2023-08-18 09:47:22 -0400

    added lock to prevent missing accounts on race

--- Scripts/Partner - Customer Perspective/Partner-Customer Account Tagger.py
+++ Scripts/Partner - Customer Perspective/Partner-Customer Account Tagger.py
@@ -2,7 +2,8 @@ import http.client
 import json
 import ssl
 import time
-from concurrent.futures import ThreadPoolExecutor, as_completed, wait
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import threading
 import config
 
 api_key = config.api_key
@@ -95,70 +96,124 @@ def fetch_customer_account_mapping(partnerapikey, max_threads=5):
     total_accounts = 0
     customers_with_accounts = 0
     
+    # Locks for thread safety
+    total_accounts_lock = threading.Lock()
+    customers_with_accounts_lock = threading.Lock()
+    mapping_lock = threading.Lock()
+
     def process_customer(customer):
         nonlocal customers_with_accounts, total_accounts
 
         print(f"Processing customer {customer.id}: {customer.name}")
-        account_list = AccountList(partnerapikey, customer.id)
-        
-        if account_list:
-            customers_with_accounts += 1
-            total_accounts += len(account_list)
+        try:
+            account_list = AccountList(partnerapikey, customer.id)
+        except Exception as e:
+            print(f"Error fetching account list for customer {customer.id}: {e}")
+            return
 
-        for account in account_list:
-            mapping[account.uniqueAccountID] = customer.name
+        if account_list:
+            with customers_with_accounts_lock:
+                customers_with_accounts += 1
+            with total_accounts_lock:
+                total_accounts += len(account_list)
+
+            # Update mapping
+            with mapping_lock:
+                for account in account_list:
+                    mapping[account.uniqueAccountID] = customer.name
     
-    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+    # Using ThreadPoolExecutor for parallel processing
+    with ThreadPoolExecutor(max_workers=max_threads) as executor:
         futures = [executor.submit(process_customer, customer) for customer in customer_list]
-        
-        # Wait for all tasks to complete
-        wait(futures)
-    
+
+        # Handle results as they come in
+        for future in as_completed(futures):
+            # This is just to catch exceptions (if any) from futures
+            try:
+                future.result()
+            except Exception as e:
+                print(f"Error in processing a customer: {e}")
+                
     print(f"\nSummary:\nTotal number of accounts: {total_accounts}")
     print(f"Number of customers with accounts: {customers_with_accounts}")
     
     return mapping
 
-def add_cloudhealth_tag(api_key, asset_list, client_api_id=0):
+def add_cloudhealth_tag(api_key, asset_list):
+    max_workers=5
+    max_retries=1
+    delay=.1
+    concurrent_tags = 100
     if len(asset_list) == 0:
         raise Exception('No assets provided to tagging function')
-    else:
-        base_url = 'chapi.cloudhealthtech.com'
-        query = '/v1/custom_tags'
-        if client_api_id:
-            query = query + f'?client_api_id={client_api_id}'
-        headers = {'Content-type': 'application/json', 'Authorization': f'Bearer {api_key}'}
+    
+    def process_assets(assets):
+        data = {"tag_groups": []}
+        for asset in assets:
+            found = False
+            for group in data["tag_groups"]:
+                if group["asset_type"] == asset['asset_type'] and group["tags"] == [{"key": asset['tag_key'], "value": asset['tag_value']}]:
+                    found = True
+                    if asset['asset_id'] not in group["ids"]:
+                        group["ids"].append(asset['asset_id'])
+            if not found:
+                data["tag_groups"].append({
+                    "asset_type": asset['asset_type'],
+                    "ids": [asset['asset_id']], 
+                    "tags": [{"key": asset['tag_key'], "value": asset['tag_value']}]
+                })
+        body = json.dumps(data)
 
-        for i in range(0, len(asset_list), 100):
-            # Get the next 100 assets from the list
-            assets = asset_list[i:i + 100]
-            data = {"tag_groups": []}
-            for asset in assets:
-                found = False
-                for group in data["tag_groups"]:
-                    if group["asset_type"] == asset['asset_type'] and group["tags"] == [{"key": asset['tag_key'], "value": asset['tag_value']}]:
-                        found = True
-                        if asset['asset_id'] not in group["ids"]:
-                            group["ids"].append(asset['asset_id'])
-                if not found:
-                    data["tag_groups"].append({
-                        "asset_type": asset['asset_type'],
-                        "ids": [asset['asset_id']], 
-                        "tags": [{"key": asset['tag_key'], "value": asset['tag_value']}]
-                    })
-            body = json.dumps(data)
+        # Retry mechanism
+        last_error = None  # To store the last error message
+        for _ in range(max_retries):
             connection = http.client.HTTPSConnection(base_url, context=ssl._create_unverified_context())
             connection.request('POST', url=query, body=body, headers=headers)
             response = connection.getresponse()
             response_body = json.loads(response.read().decode())
-            print(response.status)
+            print(response.status, response_body)
             connection.close()
-            if response_body['errors'] == []:
-                return response_body['updates']
-            try:
-                print("Error Response from tagging function:", response_body['errors'])
-            except KeyError:
-                raise Exception("There was an error with the tagging function")
+
+            if response.status == 200:
+                if response_body['errors'] == []:
+                    return response_body['updates']
+                try:
+                    last_error = response_body['errors']
+                    print("Error Response from tagging function:", last_error)
+                except KeyError:
+                    last_error = "There was an error with the tagging function"
+                    raise Exception(last_error)
+            else:
+                last_error = f"{response.status}: {response.reason}"
+
+            time.sleep(delay)  # Delay before next retry
+
+        # If we reach this point, it means max retries were hit
+        print("Max retries were exceeded for this request.")
+        print(f"{response.status}: {last_error}")
+        print("Body of Request:")
+        print(body)
+
+        return None
+
+    base_url = 'chapi.cloudhealthtech.com'
+    query = '/v1/custom_tags'
+    headers = {'Content-type': 'application/json', 'Authorization': f'Bearer {api_key}'}
+
+    # Using ThreadPoolExecutor for parallel processing
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for i in range(0, len(asset_list), concurrent_tags):
+            # Get the next {concurrent_tags} assets from the list
+            assets = asset_list[i:i + concurrent_tags]
+            futures.append(executor.submit(process_assets, assets))
+
+        results = []
+        for future in as_completed(futures):
+            result = future.result()
+            if result is not None:
+                results.append(result)
+        return results
 
 partner_accounts = AccountList(api_key, 'Partner')
 customer_account_lookup_table = fetch_customer_account_mapping(api_key)
